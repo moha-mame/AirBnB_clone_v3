c0204abba7eaf746bdf32551c36f69d9be839d39
moha-mame <>
2023-04-29 16:11:56 +0100

    AirBnB_clone_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,11 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats')
 def stats():
