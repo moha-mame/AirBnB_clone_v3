a4bf571c74cadba6898ffcfa5edc2ddc9fde417b
moha-mame <>
2023-04-29 07:53:23 +0100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_context(exception):
     """Calls storage.close() at the end of the request"""
     storage.close()
+
 
 if __name__ == "__main__":
     if getenv("HBNB_API_HOST"):
