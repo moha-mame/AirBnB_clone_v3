4cdb4ac53db1b070d40e279db1dd8f42a66829d8
moha-mame <>
2023-04-29 16:05:07 +0100

    AirBnB_clone_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,26 @@
 """JSON file status """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """
+    Returns the count of all objects by type
+    """
+    classes = {
+        'amenities': 'Amenity',
+        'cities': 'City',
+        'places': 'Place',
+        'reviews': 'Review',
+        'states': 'State',
+        'users': 'User'
+    }
+    counts = {}
+    for key, value in classes.items():
+        counts[key] = storage.count(value)
+    return jsonify(counts)
