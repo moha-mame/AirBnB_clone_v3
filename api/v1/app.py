c152e5349d94b38b88dd8d336da8f5e8171d8f46
moha-mame <>
2023-04-29 08:26:40 +0100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Flask web application"""
 
-from flask import Flask
+from flask import Flask, abort, jsonify
 from api.v1.views import app_views
 from models import storage
 from os import getenv
@@ -16,6 +16,11 @@ app.register_blueprint(app_views)
 def teardown_context(exception):
     """Calls storage.close() at the end of the request"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
