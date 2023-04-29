7af86115b8d938d440e860346ea6175bf2b08597
moha-mame <>
2023-04-29 14:52:31 +0100

    AirBnB_clone_v3

---
+++ api/v1/views/states.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+Handles all default RESTful API actions for State objects
+"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage, State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_all_states():
+    """Retrieves the list of all State objects"""
+    states = storage.all(State).values()
+    state_list = []
+    for state in states:
+        state_list.append(state.to_dict())
+    return jsonify(state_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state_by_id(state_id):
+    """Retrieves a State object by its id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """Deletes a State object by its id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Creates a State object"""
+    req_json = request.get_json()
+    if req_json is None:
+        abort(400, "Not a JSON")
+    if 'name' not in req_json:
+        abort(400, "Missing name")
+    state = State(**req_json)
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Updates a State object by its id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    req_json = request.get_json()
+    if req_json is None:
+        abort(400, "Not a JSON")
+    for key, value in req_json.items():
+        if key not in ('id', 'created_at', 'updated_at'):
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
