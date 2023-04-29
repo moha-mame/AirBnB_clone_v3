43c3b9b4983083980bfa4222cd23ffd6901c2570
moha-mame <>
2023-04-29 16:53:56 +0100

    AirBnB_clone_v3

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -26,7 +26,7 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-app_views.route("/amenities/<amenity_id>",
+@app_views.route("/amenities/<amenity_id>",
                  methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """Deletes a Amenity object"""
