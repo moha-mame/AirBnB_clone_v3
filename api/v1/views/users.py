a9092dd5437a36ca24a5513320b206e900712322
moha-mame <>
2023-05-01 07:40:06 +0100

    AirBnB_clone_v3

---
+++ api/v1/views/users.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+Defines the views for the User object RESTful API actions
+"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'])
+def get_users():
+    """Retrieves the list of all User objects"""
+    users = storage.all(User).values()
+    return jsonify([user.to_dict() for user in users])
+
+
+@app_views.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    """Retrieves a User object"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """Deletes a User object"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/users', methods=['POST'])
+def create_user():
+    """Creates a User"""
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'email' not in request.get_json():
+        abort(400, 'Missing email')
+    if 'password' not in request.get_json():
+        abort(400, 'Missing password')
+    user = User(**request.get_json())
+    storage.new(user)
+    storage.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    """Updates a User object"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    ignore = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in request.get_json().items():
+        if key not in ignore:
+            setattr(user, key, value)
+    storage.save()
+    return jsonify(user.to_dict())
