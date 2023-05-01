df872066e3f9354f0abea7b379effcc17494e022
moha-mame <>
2023-05-01 06:22:14 +0100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,16 +21,10 @@ def teardown_context(exception):
 @app.errorhandler(404)
 def not_found(error):
     """Handler for 404 errors"""
-    return jsonify({"error": "Not found"}), 404
+    return (jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    if getenv("HBNB_API_HOST"):
-        host = getenv("HBNB_API_HOST")
-    else:
-        host = '0.0.0.0'
-    if getenv("HBNB_API_PORT"):
-        port = getenv("HBNB_API_PORT")
-    else:
-        port = '5000'
+    host = getenv("HBNB_API_HOST", '0.0.0.0')
+    port = getenv("HBNB_API_PORT", '5000')
     app.run(host=host, port=port, threaded=True, debug=True)
