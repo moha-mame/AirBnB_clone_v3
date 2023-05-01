46d7efc4ee1110fba0641e041a7d952dc65e092f
moha-mame <>
2023-05-01 07:46:36 +0100

    AirBnB_clone_v3

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -9,14 +9,14 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/users', methods=['GET'])
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
     """Retrieves the list of all User objects"""
     users = storage.all(User).values()
     return jsonify([user.to_dict() for user in users])
 
 
-@app_views.route('/users/<user_id>', methods=['GET'])
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """Retrieves a User object"""
     user = storage.get(User, user_id)
@@ -25,7 +25,8 @@ def get_user(user_id):
     return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'])
+@app_views.route("users/<user_id>",
+                 methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """Deletes a User object"""
     user = storage.get(User, user_id)
@@ -36,7 +37,7 @@ def delete_user(user_id):
     return jsonify({})
 
 
-@app_views.route('/users', methods=['POST'])
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
     """Creates a User"""
     if not request.get_json():
@@ -51,7 +52,7 @@ def create_user():
     return jsonify(user.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'])
+@app_views.route("/users/<user_id>", methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """Updates a User object"""
     user = storage.get(User, user_id)
@@ -64,4 +65,4 @@ def update_user(user_id):
         if key not in ignore:
             setattr(user, key, value)
     storage.save()
-    return jsonify(user.to_dict())
+    return jsonify(user.to_dict()) 200
